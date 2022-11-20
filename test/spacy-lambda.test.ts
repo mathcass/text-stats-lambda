bb1aef6bb42ed54559008c5f89a00febcea3aa15
Cass Petrus <cass@petrus.me>
2022-11-19 18:39:56 -0800

    Initial commit

---
+++ test/spacy-lambda.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SpacyLambda from '../lib/spacy-lambda-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/spacy-lambda-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SpacyLambda.SpacyLambdaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
